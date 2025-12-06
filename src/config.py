1a27135f7f3282f1a41fd5ac086a20b27b0ae3d9
WinnerBishal <winnerbishal9848@gmail.com>
2025-12-06 00:27:32 -0600

    config data

---
+++ src/config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+@dataclass
+class TransformerConfig:
+    embed_dim: int = 768
+    qk_dim: int = 100
+    v_dim: int = 48
+    n_heads: int = 8
+
+@dataclass
+class PatcherConfig:
+    patch_size: int = 16
+    in_channels: int = 3
+    img_size: int = 224
+    embed_dim: int = 768
+
+@dataclass
+class VEncoderConfig:
+    depth: int = 12
+    n_classes: int = 1000
+
+
+
+
+
+
+
